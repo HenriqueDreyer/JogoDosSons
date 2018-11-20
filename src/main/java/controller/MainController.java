bc08fa0adbab3d1ae803e37b60f476f1ef3889ea
Henrique Dreyer <henriquedreyer@gmail.com>
2018-11-19 22:33:05 -0300

    19/11/2018 22:32 - MainController - Inclusão do novo layout

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,22 +1,19 @@
 package controller;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
+//import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseDragEvent;
 import model.Animal;
-import services.AnimalService;
 import services.ImageService;
 import services.SomService;
-
-import java.awt.event.MouseEvent;
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
+import javafx.scene.control.Label;
+import javafx.scene.effect.BlendMode;
 
 public class MainController implements Initializable {
 
@@ -27,6 +24,8 @@ public class MainController implements Initializable {
     private static Animal animal3;
     private static Animal animal4;
 
+    public int somA, imagemA;
+    
     @FXML Button buttonSom1;
     @FXML Button buttonSom2;
     @FXML Button buttonSom3;
@@ -38,21 +37,32 @@ public class MainController implements Initializable {
     @FXML ImageView animal1Image;
     @FXML ImageView animal2Image;
     @FXML ImageView animal3Image;
-    @FXML ImageView animal4Image;
-
+    @FXML ImageView animal4Image;    
+    @FXML Label labelCorreto1, labelCorreto2, labelCorreto3, labelCorreto4;
+    
     public void initialize(URL location, ResourceBundle resources) {
-        gameController = GameController.getInstance();
-
+        gameController = GameController.getInstance();        
+        
         this.carregarAnimais();
-
+        
         animal1Image.setImage(ImageService.getAnimalImage(animal1));
         animal2Image.setImage(ImageService.getAnimalImage(animal2));
         animal3Image.setImage(ImageService.getAnimalImage(animal3));
         animal4Image.setImage(ImageService.getAnimalImage(animal4));
-
+        
+        buttonAnimal1.setOnAction(new EventHandler<ActionEvent>() {            
+            public void handle(ActionEvent event) {                                                                                                
+                labelCorreto1.setVisible(true);
+                animal1Image.setBlendMode(BlendMode.LIGHTEN);
+        //        buttonAnimal1.setDisable(true);  
+        //        imagemA = animal1.getId();
+            }
+        });
+        
         buttonSom1.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
+            public void handle(ActionEvent event) {                
                 SomService.playSom(animal1);
+                //somA = animal1.getId();
             }
         });
 
@@ -76,9 +86,9 @@ public class MainController implements Initializable {
     }
 
     private void carregarAnimais() {
-        this.animal1 = gameController.getRandomAnimal();
-        this.animal2 = gameController.getRandomAnimal();
-        this.animal3 = gameController.getRandomAnimal();
-        this.animal4 = gameController.getRandomAnimal();
+        MainController.animal1 = gameController.getRandomAnimal();
+        MainController.animal2 = gameController.getRandomAnimal();
+        MainController.animal3 = gameController.getRandomAnimal();
+        MainController.animal4 = gameController.getRandomAnimal();        
     }
 }
